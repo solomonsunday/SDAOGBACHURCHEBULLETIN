4249cd4567a277972bd3bb3fcfcb0de6fc41770a
solomon sunday <schinoyerem007@gmail.com>
2024-01-26 17:45:46 +0100

    little tought on admin

--- src/common/constants/navigation.tsx
+++ src/common/constants/navigation.tsx
@@ -15,12 +15,12 @@ export const DASHBOARD_SIDEBAR_LINK = [
         width={27}
         height={27}
       />
-    )
+    ),
   },
   {
-    key: "price-list",
-    label: "Price List",
-    path: "/admin/price-list",
+    key: "bulletin",
+    label: "Bulletin",
+    path: "/admin/bulletin",
     icon: (
       <Image
         src={"/assets/svgs/price-list-icon.svg"}
@@ -29,78 +29,8 @@ export const DASHBOARD_SIDEBAR_LINK = [
         width={21.75}
         height={29}
       />
-    )
+    ),
   },
-  {
-    key: "new-order",
-    label: "New Orders",
-    path: "/admin/new-order",
-    icon: (
-      <Image
-        src={"/assets/svgs/new-orders-icon.svg"}
-        className=""
-        alt="icon"
-        width={20}
-        height={23}
-      />
-    )
-  },
-  {
-    key: "pending-order",
-    label: "Pending Orders",
-    path: "/admin/pending-order",
-    icon: (
-      <Image
-        src={"/assets/svgs/pending-orders-icon.svg"}
-        className=""
-        alt="icon"
-        width={20}
-        height={23}
-      />
-    )
-  },
-  {
-    key: "order-history",
-    label: "Order History",
-    path: "/admin/order-history",
-    icon: (
-      <Image
-        src={"/assets/svgs/order-history-icon.svg"}
-        className=""
-        alt="icon"
-        width={21}
-        height={22}
-      />
-    )
-  },
-  {
-    key: "user-data",
-    label: "User Data",
-    path: "/admin/user-data",
-    icon: (
-      <Image
-        src={"/assets/svgs/user-data-icon.svg"}
-        className=""
-        alt="icon"
-        width={24}
-        height={24}
-      />
-    )
-  },
-  {
-    key: "discounts",
-    label: "Discounts",
-    path: "/admin/discounts",
-    icon: (
-      <Image
-        src={"/assets/svgs/discounts-icon.svg"}
-        className=""
-        alt="icon"
-        width={30}
-        height={28}
-      />
-    )
-  }
 ];
 
 export const DASHBOARD_SIDEBAR_BOTTOM_LINK = [
@@ -108,12 +38,12 @@ export const DASHBOARD_SIDEBAR_BOTTOM_LINK = [
     key: "settings",
     label: "Settings",
     path: "/admin/settings",
-    icon: <Cog6ToothIcon className="text-white h-6 w-6" />
+    icon: <Cog6ToothIcon className="text-white h-6 w-6" />,
   },
   {
     key: "permission",
     label: "Permission",
     path: "/admin/permission",
-    icon: <LockOpenIcon className="text-white h-6 w-6" />
-  }
+    icon: <LockOpenIcon className="text-white h-6 w-6" />,
+  },
 ];
