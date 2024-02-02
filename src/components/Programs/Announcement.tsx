4ed9395ef87866ed16bc6cec984efb0f378f8695
solomon sunday <schinoyerem007@gmail.com>
2024-02-02 09:05:58 +0100

    updated this week bulletin

--- src/components/Programs/Announcement.tsx
+++ src/components/Programs/Announcement.tsx
@@ -9,21 +9,21 @@ const Announcement = () => {
       <div className="space-y-4 dark:text-white">
         <ol className="list-decimal space-y-4">
           <li className="border-b border-orange-200 pb-2">
-            Use the Prayer box at the entrance of the Church- drop your prayer
-            requests there anytime. The Pastor and the Elders will interceed for
-            you.
+            Drop your prayer requests in the prayer box provided at the entrance
+            of the church auditorium anytime, the Pastor and the Elders will
+            interceed for you.
           </li>
           <li className="border-b border-orange-200 pb-2">
-            Our Sabbath evenings Bible studies holds every Saturday by 4:30pm.
+            Sabbath evening Bible studies holds every Saturday by 4:30pm.
           </li>
           <li className="border-b border-orange-200 pb-2">
-            Today is our New Year Thanksgiving service
+            ⁠LMC Children ministry convention holds as follows: Date: 28/3/2024
+            Venue: St. Joseph sec. School Mangoro, Lagos Registration: 5,000
           </li>
           <li className="border-b border-orange-200 pb-2">
-            The Global Ten Days of prayer has ended, we thank God for seeing us
-            through till the end and for granting our requests. Thanks to all
-            who participated both here at the church auditorium and online via
-            zoom. May the Lord hear our prayers.
+            AWM Leadership training holds as follows: Date: 27/2/2024 to
+            1/3/2024 Arrival time: 5pm Handout:5,000 P/S: Feeding is on
+            individual basis Venue: SDA church Ogba Registration: 2,500
           </li>
           <li className="border-b border-orange-200 pb-2">
             The Current Church generator does not power all the equipments in
@@ -32,41 +32,36 @@ const Announcement = () => {
             account . Seventh-day Adventist Church Ogba. 2020086453. First Bank.
           </li>
           <li className="border-b border-orange-200 pb-2">
-            Christian Home Calendars are available at N700. per copy.
-          </li>
-          <li className="border-b border-orange-200 pb-2">
             The WAD 2024 devotional reading with the theme “Complete wellness”
             is available, Price is 2,900. Grab a copy for your family and
-            friends.
+            friends
           </li>
           <li className="border-b border-orange-200 pb-2">
-            Sabbath school officers’ meeting with the LMC Sabbath School
-            Director holds tomorrow 21/1/2024 via zoom, time is 8:00pm
+            ⁠AMO monthly meeting holds tomorrow 4/2/2024 venue: Ogba church
+            Board room and Time: 9:30am
           </li>
           <li className="border-b border-orange-200 pb-2">
-            Personal Ministries Council Meeting holds today immediately after
-            divine service. Those to attend: Pastor, Treasurer and All
-            Departmental Heads.
+            Evangelism; Daniel Seminar holds from 17th - 26th February 2024.
           </li>
           <li className="border-b border-orange-200 pb-2">
-            Youth ministry: LMC AYM will hold a Master Guide Investiture service
-            on 27/1/2024
+            Today is our District Combined Service/ Sendforth of our former
+            Pastor Pst. Adekola F.M
           </li>
           <li className="border-b border-orange-200 pb-2">
-            Singles Ministry: There will be LMC ASAM first General
-            meeting/dedication of officers for the year 2024 on 27/1/2024 venue:
-            Ogba church Time: 1:30pm.
+            Family life week holds from 10-17 Feb. 2024.
           </li>
           <li className="border-b border-orange-200 pb-2">
-            The meeting of all Board members of the Churches in Ogba District
-            holds tomorrow 21/1/2024 by 10:00am here in Ogba church
+            Ogba Church Business Meeting comes up on 24/02/2024.
           </li>
           <li className="border-b border-orange-200 pb-2">
-            ⁠Union Advisory Council meeting holds as follows: 8th - 11th
-            February, 2024. Registration - #2,000 Venue: Akure Those to attend:
-            Pastors, Personal Ministry Leaders (district and local churches).
-            N/B The names of participants should be submitted latest on
-            31/1/2024 for proper documentation.
+            There will be pulpit exchange on March 2 - 16, 2024, venue is Oke
+            Ira. Let’s pray and plan for its success.
+          </li>
+          <li className="border-b border-orange-200 pb-2">
+            Please donate towards the acquisition of a permanent camp site.
+            Let’s support this project with our generous donations and pledges.
+            Account to donate to is LMC AMO Multipurpose Cooperative Society.
+            A/c No: 0727284995, GTBank.
           </li>
         </ol>
       </div>
