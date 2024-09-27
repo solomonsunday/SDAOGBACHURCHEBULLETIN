1d12dd1ad403252ad329e1fae56e52ce935efe50
solomon sunday <schinoyerem007@gmail.com>
2024-09-27 18:37:00 +0100

    community guest day program

--- src/components/Programs/DivineService.tsx
+++ src/components/Programs/DivineService.tsx
@@ -12,48 +12,62 @@ const DivineService = () => {
           <div className="col-span-3 text-sm "> Prelude</div>
           <div className="text-xs"> Organist</div>
 
-          <div className="col-span-3 text-sm">Call to Worship: (Rev 3:20)</div>
-          <div className="text-xs"> Pst. H. Kona</div>
-          <div className="col-span-3 text-sm"> Invocation</div>
+          <div className="col-span-3 text-sm">
+            Call to Worship: (Psalm 146:1-10)
+          </div>
+          <div className="text-xs"> Pst. (Dr) O. Ogbonna</div>
+          <div className="col-span-3 text-sm"> Doxology</div>
           <div className="text-xs"> Congregation </div>
+          <div className="col-span-3 text-sm"> Invocation</div>
+          <div className="text-xs"> st. (Dr) O. Ogbonna </div>
           <div className="col-span-3 text-sm">
             {" "}
-            Welcome /Opening Hymn (SDAH 258)
+            Welcome/Introduction of Officiating Ministers
           </div>
-          <div className="text-xs">Mrs. M. Alabi</div>
+          <div className="text-xs">Elder O. Alala </div>
+
+          <div className="col-span-3 text-sm">
+            {" "}
+            Welcome /Opening Hymn (SDAH 422)
+          </div>
+          <div className="text-xs">Mrs. B. Chilaka </div>
+
           <div className="col-span-3 text-sm"> Pastoral Prayer</div>
-          <div className="text-xs"> Elder A. Ajie </div>
+          <div className="text-xs"> Elder G. Adeoti</div>
           <div className="col-span-3 text-sm"> Stewardship </div>
-          <div className="text-xs">Mrs. M. Alabi</div>
+          <div className="text-xs">Elder A. Ajie</div>
           <div className="col-span-3 text-sm"> Musical Selection</div>
-          <div className="text-xs">Mrs. M. Alabi</div>
+          <div className="text-xs">Deacon I.Oyeniyi</div>
 
           {/* <div className="col-span-3 text-sm">
             Induction: (Adventurer & Pathfinder Clubs)
           </div>
           <div className="text-xs">MG. F.L. Alonge </div> */}
           <div className="col-span-3 text-sm">
-            Scripture Reading (1 Samuel 3:10)
+            Scripture Reading (Luke 12:35-40)
           </div>
-          <div className="text-xs">Elder A. Ajie</div>
+          <div className="text-xs"> Elizabeth Ihesiaba</div>
           <div className="col-span-3 text-sm">
-            Hymn of Consecration (Master Speak)
+            Hymn of Consecration (SDAH 593)
           </div>
-          <div className="text-xs">Elder A. Ajie</div>
+          <div className="text-xs">Mrs. B. Chilaka</div>
           <div className="col-span-3 text-sm">
             Sermon Title{" "}
-            <span className="font-bold">Does the Lord still speak to Us</span>
+            <span className="font-bold text-orange-500">
+              {" "}
+              Hope Beyond the Present Crises
+            </span>
           </div>
-          <div className="text-xs">Pst. H. Kona </div>
+          <div className="text-xs"> Pst. (Dr) O. Ogbonna </div>
 
-          {/* <div className="text-xs">Man Part 2 </div>
           <div className="col-span-3 text-sm">
-            (Anchor – Elder E.J. Agbara){" "}a
-          </div> */}
+            Appreciation/Closing Hymn(SDAH 483)
+          </div>
+          <div className="text-xs">Elder O Alala</div>
           <div className="col-span-3 text-sm">Closing Hymn (SDAH 330) </div>
           <div className="text-xs">Mrs. M. Alabi</div>
           <div className="col-span-3 text-sm">Benediction</div>
-          <div className="text-xs">Pst. H. Kona</div>
+          <div className="text-xs"> Pst. (Dr) O. Ogbonna</div>
           <div className="col-span-3 text-sm">Doxology</div>
           <div className="text-xs"> Congregation</div>
         </div>
