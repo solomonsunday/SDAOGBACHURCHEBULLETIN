cb8a0e7a2784ac89a25a68418afaad5609067385
solomon sunday <schinoyerem007@gmail.com>
2024-09-28 22:10:53 +0100

    add function to get bulletin closest to todays date

--- src/common/hooks/useFilteredPublishedBulletin.tsx
+++ src/common/hooks/useFilteredPublishedBulletin.tsx
@@ -2,18 +2,13 @@
 import { useEffect, useState } from "react";
 import { BulletinStatusEnum, IBulletin } from "../interfaces";
 import { useGetbulletins } from "@/hooks/useGetBulletins";
-import { useGetBulletinById } from "@/hooks/useGetBulletinById";
 
 const useFilteredPublishedBulletin = () => {
   const [publishedData, setPublishedData] = useState<IBulletin>();
-  const [publishedBulletinId, setPublishedBulletinId] = useState<string>("");
-
   const { fetchBulletins, loading, bulletins } = useGetbulletins();
-  const { fetchBulletinById, bulletin, isLoading } = useGetBulletinById();
-
-  console.log(bulletins, "bulletins");
 
   const currentDate = new Date().toISOString();
+  const today = new Date().getTime();
 
   useEffect(() => {
     fetchBulletins({ current_date: currentDate });
@@ -24,22 +19,21 @@ const useFilteredPublishedBulletin = () => {
 
     const publishedBulletin = bulletins.filter(
       (bulletin) => bulletin.status === BulletinStatusEnum.PUBLISHED
-    );
-    const bulletin = publishedBulletin[0];
+    ); // Pick all the bulletin whose status is published
+    const bulletin = publishedBulletin.reduce((closest, current) => {
+      const currentDiff = Math.abs(+new Date(current.startDate) - today);
+      const closestDiff = Math.abs(+new Date(closest.startDate) - today);
+
+      return currentDiff < closestDiff ? current : closest;
+    }); // then from the list of the published, select the bulletin which start date is closer to today's date
+
     if (bulletin) {
-      // setPublishedBulletinId(bulletinId);
       setPublishedData(bulletin);
     }
   }, [bulletins]);
 
-  // useEffect(() => {
-  //   fetchBulletinById(publishedBulletinId);
-  //   setPublishedData(bulletin);
-  // }, [publishedBulletinId]);
-
   return {
     loading,
-    isLoading,
     publishedData,
   };
 };
