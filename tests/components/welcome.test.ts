d0f827249de880a380f4b85887c3913e40eaa972
solomon sunday <schinoyerem007@gmail.com>
2024-10-22 12:31:08 +0100

    implementing test

---
+++ tests/components/welcome.test.ts
@@ -0,0 +1,9 @@
+import { render } from "react-dom";
+import { it, expect, describe } from "vitest";
+
+describe("group", () => {
+  it("should render the welcome data when publishedData is provided", () => {
+    render(<Welcome)
+    expect(1).toBeTruthy();
+  });
+});
