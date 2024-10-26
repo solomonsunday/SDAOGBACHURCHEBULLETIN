0ce70503edc2418c8cfe073b23f1eb927efef2cf
solomon sunday <schinoyerem007@gmail.com>
2024-10-26 08:11:21 +0100

    testing in progress

--- tests/components/welcome.test.tsx
+++ tests/components/welcome.test.tsx
@@ -1,9 +1,10 @@
-import { render } from "react-dom";
 import { it, expect, describe } from "vitest";
+import { render } from "@testing-library/react";
+import Welcome from "../../src/components/Programs/Welcome";
 
 describe("group", () => {
   it("should render the welcome data when publishedData is provided", () => {
-    render(<Welcome)
+    render(<Welcome />);
     expect(1).toBeTruthy();
   });
 });
