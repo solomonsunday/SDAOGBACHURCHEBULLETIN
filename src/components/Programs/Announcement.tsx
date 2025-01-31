2f8e6c9c3366ac4efc86c6cd454e11d09ee4eb9a
solomon sunday <schinoyerem007@gmail.com>
2025-01-31 10:44:31 +0000

    updated announcement

--- src/components/Programs/Announcement.tsx
+++ src/components/Programs/Announcement.tsx
@@ -31,22 +31,32 @@ const Announcement = () => {
           </li>
 
           <li className="pb-2 border-b border-orange-200">
-            The annual Global 10 Days of Prayer program ended yesterday and
-            today is the celebration Sabbath for it's success and answered
-            prayers
+            <span className="font-bold">
+              Welcome to the ordination service of the following Pastors.
+              Adekanye S.A, Babalola V. A, Happy Kona and Majolagbe T.C.
+              Congratulations and glory to God.
+            </span>
           </li>
           <li className="pb-2 border-b border-orange-200">
-            Today is our Children Department New Curriculum Launch
+            <span className="font-bold">ADVENTIST MEN ORGANIZATION:</span>
+            AMO monthly meeting holds tomorrow 02/02/2025 Venue: boardroom
+            Time:10:00am
           </li>
 
           <li className="pb-2 border-b border-orange-200">
-            The planned All Night of prayer program for tonight has been
-            postponed till further notice.
+            Parents are reminded to make payment for the Alive in Jesus
+            neckerchief given to their children. Please see Mrs. Idowu Loveth
           </li>
 
           <li className="pb-2 border-b border-orange-200">
-            LMC day of fasting and prayer holds tomorrow Date: 19/1/2025. Time:
-            9:00am Venue: SDA Church Ogba.
+            The church leadership is calling for a regular donation of snacks
+            and support from members for our children in Baby steps and
+            beginners class upstairs.
+          </li>
+          <li className="pb-2 border-b border-orange-200">
+            There is a TMI call for prayer every morning by 5:00am - 5:30am
+            throughout the month of February for the success of the upcoming WAD
+            IMPACT evangelism
           </li>
 
           <li className="pb-2 border-b border-orange-200">
@@ -64,25 +74,15 @@ const Announcement = () => {
           <li className="pb-2 border-b border-orange-200">
             Global Youth Day comes up 9/2/2025.
           </li>
-
-          <li className="pb-2 border-b border-orange-200">
-            WAD Impact Advisory for all Pastors, First Elders and Personal
-            Ministry leaders holds at Ede. Date: Jan. 29- Feb. 1, 2025.
-          </li>
           <li className="pb-2 border-b border-orange-200">
             WAD impact evangelism holds next year, March 2 - 15 2025 Ogba
             District now has 2 centers, Members are encouraged to support this
             program..
           </li>
+
           <li className="pb-2 border-b border-orange-200">
-            Pastors Ordination: There will be ordination service on February 1,
-            2025 for the following Pastors: Adekanye S.A., Babalola V.A, Happy
-            Kona and Majolagbe T.C. Congratulations.
-          </li>
-          <li className="pb-2 border-b border-orange-200">
-            Family life and Christian home week comes up Feb. 9 - 15, 2025. This
-            will climax with the family life and Christian home Sabbath
-            celebration on 15/2/2025
+            Family life and Christian home week comes up Feb. 9 - 15, 2025. It
+            is now to be home fellowship based
           </li>
           <li className="pb-2 border-b border-orange-200">
             There will be Ogba church at business meeting on 22/2/2025.
@@ -95,24 +95,28 @@ const Announcement = () => {
           </li>
 
           <li className="pb-2 border-b border-orange-200">
-            GC Secretary of SDA Church visits Nigeria with details as follows:
-            Date: 6/2/2025 Time: 9:00am- 12:00noon. Welcome/reception venue: SDA
-            Church Maryland. N/B: All Church Clerks, Pastors, and Conference
-            officers are to be present, members are also encouraged to be in
-            attendance.
+            Special meeting with the GC Secretary of SDA Church, Pastor Erton-C
+            Kohler on mission focus, evangelism, membership nurturing and
+            retention holds as follows: Date: 7/2/2025 Time: 9:00am- 12:00noon.
+            Venue: SDA Church Maryland. N/B: All Church Clerks, Pastors, and
+            Conference officers are to be present, members are also encouraged
+            to be in attendance.
           </li>
-
           <li className="pb-2 border-b border-orange-200">
-            <span className="font-bold">LMC CHILDREN DEPARTMENT:</span> Children
-            Department New Curriculum Launch is next Sabbath 18/1/2025 Zonal
-            Quiz: Ogba zone (Ogba, Iju and Ojokoro District) LMC children
-            department quiz holds as follows: Date: 20/4/2025. Venue: SDA Church
-            Ogba. Material for Study: Michael Asks Why. Beginner(1-3yrs): God's
-            words. Kindergarten (4-6yrs): Our worst enemy. Primary (7-9yrs):
-            Where did sin come from? and Can the dead speak? Power Point
-            (10-12yrs): God's unchanging law and Death and Resurrection. Real
-            time and Cornerstone will study the Book of Daniel- complete study,
-            Images and symbols.
+            LMC Elders Certification comes up April 13, 2025. All Elders are to
+            prepare for this by studying the Elders' Handbook.
+          </li>
+          <li className="pb-2 border-b border-orange-200">
+            <span className="font-bold">
+              LMC CHILDREN DEPARTMENT {"=>"} Zonal Quiz:
+            </span>{" "}
+            Ogba zone (Ogba, Iju and Ojokoro District) LMC children department
+            quiz holds as follows: Date: 20/4/2025. Venue: SDA Church Ogba.
+            Material for Study: Michael Asks Why. Beginner(1-3yrs): God's words.
+            Kindergarten (4-6yrs): Our worst enemy. Primary (7-9yrs): Where did
+            sin come from? and Can the dead speak? Power Point (10-12yrs): God's
+            unchanging law and Death and Resurrection. Real time and Cornerstone
+            will study the Book of Daniel- complete study, Images and symbols.
           </li>
           <li className="pb-2 border-b border-orange-200">
             Keep the Church environment clean. Dispose every trash properly in
