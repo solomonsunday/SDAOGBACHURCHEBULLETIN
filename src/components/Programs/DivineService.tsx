e9cc1ec6eb52930472fe51afc9fe72ca1c88db93
solomon sunday <schinoyerem007@gmail.com>
2025-01-31 12:48:39 +0000

    updated announcement and devine service

--- src/components/Programs/DivineService.tsx
+++ src/components/Programs/DivineService.tsx
@@ -11,38 +11,79 @@ const DivineService = () => {
         <div className="grid grid-cols-4 gap-2">
           <div className="col-span-3 text-sm "> Prelude</div>
           <div className="text-xs"> Organist/Student Praise Team</div>
-          <div className="col-span-3 text-sm">Call to Worship: (Jeremiah 29:12-13)</div>
-          <div className="text-xs">Elder Gbenga Adeoti</div>
-          <div className="col-span-3 text-sm"> Invocation</div>
-          <div className="text-xs">Congregation</div>
-          <div className="col-span-3 text-sm">Adoration Hymn: (SDAH 251)</div>
-          <div className="text-xs">Elder Anthony Ajie</div>
-          <div className="col-span-3 text-sm"> Pastoral Prayer</div>
-          <div className="text-xs">Elder Emeka Agbara</div>
-          <div className="col-span-3 text-sm"> Stewardship </div>
-          <div className="text-xs">Deacon Chimezie Mbagwu</div>
           <div className="col-span-3 text-sm">
+            Call to Worship: (Jeremiah 29:12-13)
+          </div>
+          <div className="text-xs">Pastor Adeniyi E O</div>
+          <div className="col-span-3 text-sm"> Invocation</div>
+          <div className="text-xs">Pastor Adeleye E A</div>
+          <div className="col-span-3 text-sm"> Introduction</div>
+          <div className="text-xs">Pastor Salami A A</div>
+
+          <div className="col-span-3 text-sm">Adoration Hymn: (SDAH 088)</div>
+          <div className="text-xs">Pastor Alabi A D </div>
+          <div className="col-span-3 text-sm"> Pastoral Prayer</div>
+          <div className="text-xs">Pastor Oyinloye E A</div>
+          <div className="col-span-3 text-sm"> Stewardship/Offertory </div>
+          <div className="text-xs">Elder Edafe D</div>
+
+          <div className="col-span-3 text-sm">
+            {" "}
+            Presentation of Ordination candidates{" "}
+          </div>
+          <div className="text-xs">Pastor Salami A A</div>
+          <div className="col-span-3 text-sm">Prayer and Laying of Hands </div>
+          <div className="text-xs">Pastor Adeleye E A</div>
+          <div className="col-span-3 text-sm">Charges</div>
+          <div className="text-xs">Pastor Adeleye E A/Oyinloye E A</div>
+          <div className="col-span-3 text-sm">
+            Welcoming Newly Ordained Pastors{" "}
+          </div>
+          <div className="text-xs">Pastor Uchegbue U O </div>
+          <div className="col-span-3 text-sm">
+            Welcoming Newly Ordained Pastor’s Wives
+          </div>
+          <div className="text-xs">Shepherdess Chigozirim B Umez- Uchegbue</div>
+          <div className="col-span-3 text-sm">Dress Up for Mission</div>
+          <div className="text-xs">Pastor Adeniyi E O/ Alabi, A.D.</div>
+          <div className="col-span-3 text-sm">
+            Special Song – Shepherdess International{" "}
+          </div>
+          <div className="text-xs">Elder Adedokun A B</div>
+
+          <div className="col-span-3 text-sm">
+            Response – Newly Ordained Pastors
+          </div>
+          <div className="text-xs">Pastor Adeniyi E O/Alabi A D</div>
+          <div className="col-span-3 text-sm">Choir Ministeration </div>
+          <div className="text-xs">Elder Adedokun A B</div>
+          {/* <div className="col-span-3 text-sm">
             {" "}
             Musical Selection: (Ogba Choir){" "}
           </div>
-          <div className="text-xs"> Deacon Chimezie Mbagwu</div>
-          <div className="col-span-3 text-sm">Scripture Reading: (Luke 11:1)</div>
-          <div className="text-xs">Sister Jesutofunmi Olusona</div>
+          <div className="text-xs"> Deacon Chimezie Mbagwu</div> */}
           <div className="col-span-3 text-sm">
-            Hymn of Consecration (SDAH 290)
+            Scripture Reading: (2 Timothy 4:1-5)
           </div>
-          <div className="text-xs">Sister Jesutofunmi Olusona</div>
+          <div className="text-xs">Pastor Oyinloye E A</div>
+          <div className="col-span-3 text-sm">
+            Hymn of Consecration (SDAH 319)
+          </div>
+          <div className="text-xs"> Elder Edafe D </div>
           <div className="col-span-3 text-sm">
             Sermon Title:{" "}
-            <span className="font-bold text-orange-500"> “But when you Pray”</span>
+            <span className="font-bold text-orange-500">
+              {" "}
+              “NO EXCUSE FOR MISSION, PREACH THE WORD.”
+            </span>
           </div>
-          <div className="text-xs">Elder Gbenga Adeoti</div>
-          <div className="col-span-3 text-sm">Closing Hymn: (SDAH 518)</div>
-          <div className="text-xs">Elder Emeka Agbara</div>
+          <div className="text-xs">Pastor Adeleye, E.A.</div>
+          <div className="col-span-3 text-sm">Closing Hymn: (SDAH 430)</div>
+          <div className="text-xs">Pastor Salami A A</div>
           <div className="col-span-3 text-sm">
             Benediction / Prayer for the Church
           </div>
-          <div className="text-xs"> Elder Gbenga Adeoti</div>
+          <div className="text-xs"> Pastor Adeleye E A</div>
           <div className="col-span-3 text-sm">Doxology</div>
           <div className="text-xs"> Congregation</div>
         </div>
